f5859e11c853fc916cf4c0d67efdf92d51323fa6
twish6peeka <109449092+twish6peeka@users.noreply.github.com>
2026-02-01 15:53:07 +0530

    Temporary tables for data cleaning/loading

---
+++ sql/02_staging.sql
@@ -0,0 +1,85 @@
+-- ==========================================
+-- 02_staging.sql
+-- Temporary staging tables for cleaning
+-- ==========================================
+
+-- Drop tables if they exist (reset)
+DROP TABLE IF EXISTS customers_staging;
+DROP TABLE IF EXISTS orders_staging;
+DROP TABLE IF EXISTS order_items_staging;
+DROP TABLE IF EXISTS payments_staging;
+DROP TABLE IF EXISTS reviews_staging;
+DROP TABLE IF EXISTS sellers_staging;
+DROP TABLE IF EXISTS geolocation_staging;
+
+-- Customers staging
+CREATE TABLE customers_staging (
+    customer_id TEXT,
+    full_name TEXT,
+    email TEXT,
+    signup_date TEXT,
+    country TEXT,
+    customer_zip_code_prefix TEXT
+);
+
+-- Orders staging
+CREATE TABLE orders_staging (
+    order_id TEXT,
+    customer_id TEXT,
+    order_status TEXT,
+    order_purchase_timestamp TIMESTAMP,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP
+);
+
+-- Order items staging
+CREATE TABLE order_items_staging (
+    order_id TEXT,
+    order_item_id INT,
+    product_id TEXT,
+    seller_id TEXT,
+    shipping_limit_date TIMESTAMP,
+    price NUMERIC,
+    freight_value NUMERIC
+);
+
+-- Payments staging
+CREATE TABLE payments_staging (
+    order_id TEXT,
+    payment_sequential INT,
+    payment_type TEXT,
+    payment_installments INT,
+    payment_value NUMERIC
+);
+
+-- Reviews staging
+CREATE TABLE reviews_staging (
+    review_id TEXT,
+    order_id TEXT,
+    review_score INT,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date TIMESTAMP,
+    review_answer_timestamp TIMESTAMP
+);
+
+-- Sellers staging
+CREATE TABLE sellers_staging (
+    seller_id TEXT,
+    seller_name TEXT,
+    seller_city TEXT,
+    seller_state TEXT,
+    seller_zip_code TEXT,
+    seller_country TEXT
+);
+
+-- Geolocation staging
+CREATE TABLE geolocation_staging (
+    geolocation_zip_code_prefix TEXT,
+    geolocation_city TEXT,
+    geolocation_state TEXT
+);
+
+
